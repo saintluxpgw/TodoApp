e412bc88b393bb0321ae11071237dc971ac98574
saintluxpgw <saintluxpgw@gmail.com>
2020-12-28 21:02:18 +0900

    Image src editing

--- bg.js
+++ bg.js
@@ -3,7 +3,7 @@ const IMG_NUMBER = 5;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `images/${imgNumber}.jpg`;
+    image.src = `/images/${imgNumber}.jpg`;
     body.appendChild(image);
 }
 
